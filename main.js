8986dce86f7999278faa8e96abf2ef49cf2489de
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-03-06 15:23:53 -0500

    commit tree 68d80df1443655914d18b761f14cb2ca3d6d0f0f

--- main.js
+++ main.js
@@ -3,9 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const selectHeart = document.querySelectorAll('.like-glyph');
+console.log(selectHeart);
 
+selectHeart.forEach(selectHeart => selectHeart.addEventListener("click", likeHeart))
 
+function likeHeart(event) {
+  const redHeart = event.target;
+  mimicServerCall()
+    .then(function(messageToServer){
+      alert(messageToServer);
+      if(redHeart.innerText === EMPTY_HEART) {
+        redHeart.classList.add("activated-heart");
+        redHeart.innerText = FULL_HEART;
+      }
+      else {
+        redHeart.classList.remove("activated-heart");
+        redHeart.innerText = EMPTY_HEART;
+      }
+      
+    })
+    .catch(function(error) {
+      alert(error);
+      const popUp = document.querySelector(".hidden");
+      popUp.classList.remove("hidden");
+      const removePopUp = document.getElementById("modal")
+      setTimeout(() => {removePopUp.classList.add("hidden")}, 3000);
+      console.log('error message sent to server')
+    });
+}
 
+// for(const liked of selectHeart) {
+//   liked.addEventListener('click', likeHeart)
+// }
+
+// for(const unLiked of selectHeart) {
+//   unLiked.addEventListener('click', unLikeHeart)
+// }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
